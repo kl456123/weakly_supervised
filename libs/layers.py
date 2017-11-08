7057877a377ff74422cbe57e905adb36b94d504a
breakpoint <kl456123@outlook.com>
2017-11-08 16:12:18 +0800

    first upload in pytorch

--- libs/layers.py
+++ libs/layers.py
@@ -4,11 +4,112 @@
 from libs.tools import _get, stop
 from libs.points import *
 import numpy as np
+import torch as T
 
 
 class Layers(object):
     def __init__(self, net):
         self.net = net
+
+    def conv_faster(self,
+                    layer_name,
+                    points,
+                    isFilter=True,
+                    test_part_pos=None):
+        layer_info = self.net.get_layer_info(layer_name)
+        K = layer_info['kernel_size']
+        P = _get(layer_info['pad'], 0)
+        S = _get(layer_info['stride'], 1)
+        D = _get(layer_info['dilation'], 1)
+        dim = points[0].dim
+        weight, bias = self.net.get_param_data(layer_name)
+        n = weight.shape[0]
+        c = weight.shape[1]
+        res = []
+        all_weight = [] all_prior = []
+        kdim = K * K
+        bottom_name = self.net._net.bottom_names[layer_name][0]
+        bottom_shape = self.net._net.blobs[bottom_name].data[self.net.img_idx].shape
+
+        for point in points:
+            pos = point.pos
+            all_prior.append(point.prior)
+            if dim == 2:
+                all_weight.append(point.weight)
+            elif dim == 3:
+                temp_weight = T.zeros(n)
+                temp_weight[int(pos[0])] = point.weight
+                all_weight.append(temp_weight)
+
+        weight = tensor(weight)
+        bias = tensor(bias)
+        all_weight = tensor(all_weight)
+        all_prior = tensor(all_prior)
+        weighted_weight = all_weight[:, :, None,
+                                     None, None] * weight[None, :, :, :, :]
+        weighted_weight = weighted_weight.sum(dim=1)
+        weighted_bias = all_weight * bias[None, :]
+        weighted_bias = weighted_bias.sum(dim=1)
+        all_prior = (weighted_bias + all_prior) / kdim
+
+        ##############################################
+        # merge points according to their pos
+        ##############################################
+        all_children_weight_pos_pairs = {}
+        all_children_prior_pos_pairs = {}
+        for idx, point in enumerate(points):
+            pos = point.pos
+            w = weighted_weight[idx]
+            p = all_prior[idx]
+            hash_pos = arr2hash(pos)
+            if hash_pos not in all_children_weight_pos_pairs:
+                all_children_weight_pos_pairs[hash_pos] = []
+                all_children_prior_pos_pairs[hash_pos] = []
+            all_children_weight_pos_pairs[hash_pos].append(w)
+            all_children_prior_pos_pairs[hash_pos].append(p)
+
+        for hash_pos in all_children_weight_pos_pairs:
+            pos = hash2arr(hash_pos)
+            children_pos = get_children_pos(pos, K, S, D, P)
+            children_weight = T.stack(all_children_weight_pos_pairs[hash_pos])
+            data = tensor(self.net.get_block_data(
+                bottom_name, children_pos, P))
+            data = data.view((-1, K, K))
+            children_conv = data[None, :, :, :] * children_weight
+            all_per_prior = tensor(all_children_prior_pos_pairs[hash_pos])
+            all_score_map = children_conv.sum(
+                dim=1) + all_per_prior[:, None, None]
+            all_score_sum = all_score_map.view((-1, kdim)).sum(dim=1)
+            threshold = all_score_sum / 9
+            if isFilter:
+                all_keep = all_score_map > threshold.view((-1, 1, 1))
+            else:
+                all_keep = all_score_map > float('-inf')
+
+            if test_part_pos is not None:
+                if all_keep[0, test_part_pos[0], test_part_pos[1]]:
+                    all_keep[0, :, :] = False
+                    part_pos = test_part_pos
+                    all_keep[0, part_pos[0], part_pos[1]] = True
+                else:
+                    return []
+
+            all_score_map = all_score_map.view(-1)
+            all_keep_weight = children_weight.permute(
+                0, 2, 3, 1).contiguous().view((-1, c))
+            num = len(all_keep)
+            all_keep = all_keep.view(-1)
+            for i in range(num):
+                if all_keep[i]:
+                    per_pos = children_pos[i % kdim]
+                    per_weight = all_keep_weight[i]
+                    per_prior = all_per_prior[i // kdim]
+                    if not check_is_pad(per_pos, bottom_shape[-2:]):
+                        res.append(Point_2D(per_pos,
+                                            per_weight,
+                                            per_prior,
+                                            all_score_map[i]))
+        return res
 
 
 # output point is 2D
@@ -81,22 +182,20 @@ class Layers(object):
         negative_slope = _get(layer_info['negative_slope'], 0)
         dim = points[0].dim
         bottom_name = self.net.get_bottom_name(layer_name)
-        data = self.net.get_block_data(bottom_name, None, 0)
+        data = tensor(self.net.get_block_data(bottom_name, None, 0))
         # data = self.net._net.blobs[bottom_name].data[self.net.img_idx]
         # res = []
         for point in points:
             pos = point.pos
             if dim == 2:
-                if len(data.shape) == 1:
-                    point.weight[np.where(data <= 0)] *= negative_slope
+                if len(data.size) == 1:
+                    point.weight[data <= 0] *= negative_slope
                 else:
-                    point.weight[np.where(data[:, int(pos[1]), int(pos[2])] <= 0)
+                    point.weight[data[:, int(pos[1]), int(pos[2])] <= 0
                                  ] *= negative_slope
             else:
                 if data[int(pos[0]), int(pos[1]), int(pos[2])] <= 0:
-                    # continue
                     point.weight *= negative_slope
-            # res.append(point)
         return points
 
     def pool_faster(self, layer_name, points, isFilter=True):
@@ -123,13 +222,6 @@ class Layers(object):
         num = points[0].weight.size
         assert dim == 2, 'dim ==3 is not supported'
         for point in points:
-            #         if dim == 2:
-                # if FC:
-                    # pass
-                # else:
-                    # pass
-            # else:
-                # pass
             pos = point.pos
             per_prior = point.prior / num
             for i in range(num):
@@ -157,85 +249,18 @@ class Layers(object):
 
         return res
 
-    def pool(self, layer_name, points, isFilter=True):
-        layer_info = self.net.get_layer_info(layer_name)
-        K = layer_info['kernel_size']
-        S = _get(layer_info.get('stride'), 1)
-        P = _get(layer_info.get('pad'), 0)
-        # output point is 3D
-        skip_merge = False
-        if len(points) == 1:
-            skip_merge = True
-        bottom_points_3D = []
-        bottom_name = self.net._net.bottom_names[layer_name][0]
-        # all point_2D must be converted to 3D before passing through pooling layer
-        top_points_3D = []
-        FC = 0
-        if np.isnan(points[0].pos[1]):
-            FC = 1
-        import time
-        start_pool = time.time()
-        if points[0].dim == 2:
-            for point in points:
-                if FC:
-                    top_name = self.net._net.top_names[layer_name][0]
-                    data = self.net._net.blobs[top_name].data[self.net.img_idx]
-                    c, h, w = data.shape
-                    num = c * h * w
-                    positions_3D = np.array(np.unravel_index(
-                        np.arange(num), data.shape)).T
-                else:
-                    c = len(point.weight)
-                    positions_3D = [(i, int(point.pos[1]), int(
-                        point.pos[2])) for i in range(c)]
-                points_3D = convert_2Dto3D(point, positions_3D)
-                top_points_3D += points_3D
-        convert_time = time.time()
-        print('convert_time :{:.5f}'.format(convert_time - start_pool))
-
-        pooling_blob_shape = self.net._net.blobs[bottom_name].data.shape[1:]
-        for point in top_points_3D:
-            if isFilter and point.weight <= 0:
-                # filter
-                continue
-            assert point.dim == 3, 'all points must be converted to 3D first'
-            # 3D
-            # just one child
-            pos = point.pos
-            c = int(pos[0])
-            h = int(pos[1])
-            w = int(pos[2])
-            # if check_is_pad(pos, pooling_blob_shape):
-            # # just used for case that pad!=0
-            # continue
-            children_pos = point.get_children_pos(K, S, D=1, P=P)
-            data = self.net.get_block_data(
-                bottom_name, children_pos, 0)
-            idx = np.argmax(data[c], axis=0)
-
-            # x, y = np.unravel_index(idx, (K, K))
-            point.pos = children_pos[idx]
-            # point_3D = Point_3D(
-            # children_pos[idx], point.weight, point.prior)
-            bottom_points_3D.append(point)
-        filter_time = time.time()
-        print('filter time:{:.5f}'.format(filter_time - convert_time))
-
-        if not skip_merge:
-            bottom_points_3D = merge_points(
-                bottom_points_3D, pooling_blob_shape)
-        return bottom_points_3D
-
     def fc(self, layer_name, points):
         # output point is 2D,just like 1x1 conv
         # now just 2D input point is supported
         assert len(points) == 1, 'just one 2D point'
         point_2D = points[0]
         weight, bias = self.net.get_param_data(layer_name)
-        weighted_weight = point_2D.weight[..., np.newaxis] * weight
-        weighted_weight = weighted_weight.sum(axis=0)
+        weight = tensor(weight)
+        bias = tensor(bias)
+        weighted_weight = point_2D.weight[:, None] * weight
+        weighted_weight = weighted_weight.sum(dim=0)
         weighted_bias = point_2D.weight * bias
-        weighted_bias = weighted_bias.sum(axis=0)
+        weighted_bias = weighted_bias.sum(dim=0)
         prior = point_2D.prior + weighted_bias
         return [Point_2D([np.nan, np.nan, np.nan], weighted_weight, prior)]
 
@@ -275,7 +300,7 @@ class Layers(object):
                     test_flag -= 1
                 else:
                     test_part_pos = None
-                points = self.conv(layer_name,
+                points = self.conv_faster(layer_name,
                                           points,
                                           isFilter,
                                           test_part_pos)
@@ -330,7 +355,7 @@ class Layers(object):
 
 def get_points_scores(net, points, layer_name):
     bottom_name = net._net.bottom_names[layer_name][0]
-    data = net._net.blobs[bottom_name].data[net.img_idx]
+    data = tensor(net._net.blobs[bottom_name].data[net.img_idx])
     dim = points[0].dim
     res = []
     FC = False
@@ -340,16 +365,16 @@ def get_points_scores(net, points, layer_name):
         pos = point.pos
         if dim == 2:
             if FC:
-                score = point.weight * data.ravel()
+                score = point.weight * data.view(-1)
             else:
                 score = point.weight * data[:, int(pos[1]), int(pos[2])]
-            score = score.sum(axis=0) + point.prior
+            score = score.sum(dim=0) + point.prior
         else:
             score = point.weight * \
                 data[int(pos[0]), int(pos[1]), int(pos[2])] +\
                 point.prior
         res.append(score)
-    res = np.array(res)
+    res = T.stack(res)
     return res
 
 
@@ -410,4 +435,10 @@ def get_children_pos(pos, K, S=1, D=1, P=0, filter_pad=False):
     return np.array(res)
 
 
-
+def tensor(arr, dtype=T.cuda.FloatTensor):
+    if isinstance(arr, list) or isinstance(arr, tuple):
+        arr = np.array(arr)
+    if isinstance(arr, np.ndarray):
+        return T.from_numpy(arr).type(dtype)
+    if T.is_tensor(arr):
+        return arr
